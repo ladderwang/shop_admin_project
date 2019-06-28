8ba89a0d21c2c32d46129e1a10fd7d2c42c6fc3a
wangwei <582532064@qq.com>
2019-06-28 16:11:15 +0800

    1、home页静态页面搭建 侧边栏和顶栏

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // 引入Login组件
 import Login from './views/Login.vue'
+// 引入Home组件
+import Home from './views/Home.vue'
 
 Vue.use(Router)
 
@@ -14,6 +16,10 @@ export default new Router({
     {
       path: '/',
       component: Login
+    },
+    {
+      path: '/home',
+      component: Home
     }
   ]
 })
